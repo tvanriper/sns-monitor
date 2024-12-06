c6fcf18ab503e190909e37e6f564cfe234907fa1
Trey Van Riper <trey@dahliahealth.com>
2024-12-06 06:42:18 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Joseph E. Van Riper III <vanriper.trey@gmail.com>
+All Rights Reserved
+*/
+package main
+
+import "github.com/tvanriper/sns-monitor/cmd"
+
+func main() {
+	cmd.Execute()
+}
